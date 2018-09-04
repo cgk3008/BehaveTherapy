131fa2614f3fe6f510b22190a02fa808c7cb49c9
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-09-04 17:43:29 -0400

    Add project files.

---
+++ BehaveTherapy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BehaveTherapy.MvcApplication" Language="C#" %>
---
+++ BehaveTherapy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BehaveTherapy.Startup))]
+namespace BehaveTherapy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
