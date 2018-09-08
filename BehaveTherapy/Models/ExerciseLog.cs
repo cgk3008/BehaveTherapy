5cb2791b0fb96f33c5dc5022a5c6a07e7583bcde
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-09-08 13:53:17 -0400

    Adding Exercise log model, then Lesson model for individuals.

---
+++ BehaveTherapy/Models/ExerciseLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BehaveTherapy.Models
+{
+    public class ExerciseLog
+    {
+        public int Id { get; set; }
+        [StringLength(100, ErrorMessage = "The {0} cannot be {1} characters long.")]
+        public string Name { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string TherapistId { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<Lesson> Lesson { get; set; }
+
+    }
+}
