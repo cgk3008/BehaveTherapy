a3ef0e2519a698715dfc2e37b6da1bd458c0abda
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-09-11 10:49:55 -0400

    setting Identity model and extension model for Users and Code first migrations.

--- BehaveTherapy/Models/IdentityModels.cs
+++ BehaveTherapy/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,11 +10,32 @@ namespace BehaveTherapy.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
+        public string FullName { get; set; }
+
+        public virtual ICollection<TherapyPlan> TherapyPlan {get; set;}
+        public virtual ICollection<Exercises> Exercises { get; set; }
+        public virtual ICollection<PlanNotifications> PlanNotifications { get; set; }
+
+        public ApplicationUser()
+        {
+            TherapyPlan = new HashSet<TherapyPlan>();
+            Exercises = new HashSet<Exercises>();
+            PlanNotifications = new HashSet<PlanNotifications>();
+        }
+
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+
+            userIdentity.AddClaim(new Claim("Name", FullName));
+
             return userIdentity;
         }
     }
@@ -29,5 +51,10 @@ namespace BehaveTherapy.Models
         {
             return new ApplicationDbContext();
         }
+
+public DbSet<TherapyPlan> TherapPlan { get; set; }
+        public DbSet<Exercises> Exercises { get; set; }
+        public DbSet<PlanNotifications> PlanNotifications { get; set; }
+
     }
 }
